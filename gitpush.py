4e333d70887c5caf7e63a7e084dd3b1e90a5f63a
p-jay <pjay1118.aws@gmail.com>
2019-11-16 20:51:19 -0600

    changes made

--- gitpush.py
+++ gitpush.py
@@ -1,1 +1,5 @@
-print ("Hey, it's a thanksgiving weekend !!!")
+holiday=input ("Thanksgiving weekend?")
+if holiday=="yes":
+    print ("come over for the weekend")
+else:
+    print ("Will catch up next week")
